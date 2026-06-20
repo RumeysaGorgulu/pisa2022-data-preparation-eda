1f440b3dd4c08269bae1df991ea8801764b65b92
Rumeysa Gorgulu <rumeysagorgulu19@gmail.com>
2026-06-20 10:38:52 +0300

    Update pisa2022_data_prep_eda.R

--- pisa2022_data_prep_eda.R
+++ pisa2022_data_prep_eda.R
@@ -129,7 +129,6 @@ country_summary <- imputed_variables %>%
 
 correlation <- cor(imputed_variables[, c("ESCS", "MATHH", "READD", "SCIEE")])
 
-
 #Saving the changes
 saveRDS(imputed_variables, "imputed_variables.rds")
 print(correlation)
